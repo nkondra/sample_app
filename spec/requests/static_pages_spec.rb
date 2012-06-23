377e366aac075f062de3b74d5e865355f8043902
Nathan Kondra <nathan@cagedpheonix.com>
2012-06-22 18:24:57 -0700

    Add a contact page for exersise 3.5.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,5 +41,18 @@ describe "Static Pages" do
 
 	end
 
+	describe "Contact Page" do
+		it "should have the content 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', text: 'Contact Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Contact Us')
+		end
+
+	end
+
 end
 
