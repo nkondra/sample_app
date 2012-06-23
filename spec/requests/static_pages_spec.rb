cdf6971d02a4d651aa8134fa7c58be3f0be979dc
Nathan Kondra <nathan@cagedpheonix.com>
2012-06-22 19:40:30 -0700

    Dry out spec's

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,54 +2,56 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
 	describe "Home Page" do
-		it "should have the content 'Sample App'" do
+		it "should have the H1 'Sample App'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', text: 'Sample App')
 		end
 
 		it "should have the right title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Home')
+			page.should have_selector('title', text: "#{base_title} | Home")
 		end
 
 	end
 
 	describe "Help Page" do
-		it "should have the content 'Help'" do
+		it "should have the H1 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('h1', text: 'Help')
 		end
 
 		it "should have the right title" do
 			visit '/static_pages/help'
-			page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Help')
+			page.should have_selector('title', text: "#{base_title} | Help")
 		end
 	
 	end
 
 	describe "About Page" do
-		it "should have the content 'About Us'" do
+		it "should have the H1 'About Us'" do
 			visit '/static_pages/about'
 			page.should have_selector('h1', text: 'About Us')
 		end
 
 		it "should have the right title" do
 			visit '/static_pages/about'
-			page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | About Us')
+			page.should have_selector('title', text: "#{base_title} | About Us")
 		end
 
 	end
 
 	describe "Contact Page" do
-		it "should have the content 'Contact Us'" do
+		it "should have the H1 'Contact Us'" do
 			visit '/static_pages/contact'
 			page.should have_selector('h1', text: 'Contact Us')
 		end
 
 		it "should have the right title" do
 			visit '/static_pages/contact'
-			page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Contact Us')
+			page.should have_selector('title', text: "#{base_title} | Contact Us")
 		end
 
 	end
